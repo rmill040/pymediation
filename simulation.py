01ce3584e6b1fc2ee49f56fc67f334b98cec8d23
Robert Milletich <R2@Roberts-Air.home>
2016-10-30 01:11:22 -0400

    fixed print in simulation.py

--- simulation.py
+++ simulation.py
@@ -15,10 +15,10 @@ def linear_to_binomial(x = None, n = None):
 	tmp = 1 / (1 + np.exp(-x))
 	return np.random.binomial(1, tmp, (n, 1))
 
-def write_estimates(i = None, N = None, a = None, b = None, c = None, method = None,
-					estimator = None, b1 = None, b2 = None, point = None, ll = None,
-					ul = None):
-	tmp = [i, N, a, b, c, method, estimator, b1, b2, point, ll, ul]
+def write_estimates(i = None, N = None, mediator_type = None, endogenous_type = None, 
+					a = None, b = None, c = None, method = None, estimator = None, 
+					b1 = None, b2 = None, point = None, ll = None, ul = None):
+	tmp = [i, N, mediator_type, endogenous_type, a, b, c, method, estimator, b1, b2, point, ll, ul]
 	print(','.join(map(str, tmp)))
 
 # Main simulation function
@@ -83,6 +83,8 @@ def simulation(iterations = None, rank = None):
 									estimates = clf.indirect_effect()
 									write_estimates(i = i,
 													N = N,
+													mediator_type = mediator_type,
+													endogenous_type = endogenous_type,
 													a = a,
 													b = b,
 													c = c,
@@ -105,6 +107,8 @@ def simulation(iterations = None, rank = None):
 									estimates = clf.indirect_effect()
 									write_estimates(i = i,
 													N = N,
+													mediator_type = mediator_type,
+													endogenous_type = endogenous_type,
 													a = a,
 													b = b,
 													c = c,
@@ -122,7 +126,6 @@ def simulation(iterations = None, rank = None):
 										b2_list = [N, 10*N, b1]
 										for b2 in b2_list:
 											for estimator in estimator_list:
-												print(method,b1, b2,estimator)
 												clf = MediationModel(method = method, 
 																	 mediator_type = mediator_type,
 																     endogenous_type = endogenous_type,
@@ -133,6 +136,8 @@ def simulation(iterations = None, rank = None):
 												estimates = clf.indirect_effect()
 												write_estimates(i = i,
 																N = N,
+																mediator_type = mediator_type,
+																endogenous_type = endogenous_type,
 																a = a,
 																b = b,
 																c = c,
@@ -146,22 +151,20 @@ def simulation(iterations = None, rank = None):
 
 def main():
 
-	# Start global timer
-	global_start = timeit.default_timer()	
-
 	# Define MPI parameters
 	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
 
 	# Set seed for each cpu
-	np.random.seed(rank*10)
+	np.random.seed(rank*10 + 1)
 
 	# Get arguments from user
 	iterations = int(sys.argv[1])
 	
 	# Run function
 	simulation(iterations = iterations, rank = rank)
-	print('\nSimulation Finished in {0} hours on process {1}\n'.format((timeit.default_timer() - global_start)/3600, rank))
 
 if __name__ == "__main__":
+	time_start = timeit.default_timer()
 	main()
+	print('Simulation finished in {0} hours'.format((timeit.default_timer() - time_start))/3600)
