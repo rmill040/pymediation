bb53175339c8305935a5313b4cebc4df7854419b
Robert Milletich <R2@Roberts-Air.home>
2017-08-12 00:05:16 -0400

    test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pymediation',
+    version='0.1dev',
+    packages=['mediation',],
+    license='None',
+    long_description=open('README.txt').read(),
+)
